175318baaf7061131ba9618631040e4aca90cb61
Jim Thompson <jimthompson5802@aol.com>
2019-07-19 19:32:07 -0400

    converted to start docker container for mlflow server

--- bin/run_mlflow_server.sh
+++ bin/run_mlflow_server.sh
@@ -2,4 +2,10 @@
 
 # run mlflow server in the docker container
 
-mlflow server --host 0.0.0.0 --backend-store-uri=file:///opt/project/tracking
+docker run --detach --rm \
+    -e MLFLOW_TRACKING_URI=${MLFLOW_TRACKING_URI} \
+    -v ${KAGGLE_PROJECT}:/opt/project \
+    -p 5000:5000 \
+    --name mlflow_server \
+    kag_python \
+    mlflow server --host 0.0.0.0 --backend-store-uri=file:///opt/project/tracking
