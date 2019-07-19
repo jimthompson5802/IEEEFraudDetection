e7b6a96a0754e62b3132301f75043a07706e32c4
Jim Thompson <jimthompson5802@aol.com>
2019-07-19 18:45:15 -0400

    corrected envrionment variable name

--- bin/run_python_container.sh
+++ bin/run_python_container.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 docker run -it --rm \
-    -e MFLOW_TRACKING_URI=${MLFLOW_TRACKING_URI} \
+    -e MLFLOW_TRACKING_URI=${MLFLOW_TRACKING_URI} \
     -v ${KAGGLE_PROJECT}:/opt/project \
     -p 5000:5000 \
     --name kag_python \
