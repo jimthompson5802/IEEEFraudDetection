dde228b4119802abf587dc8225e7e3997c49e59b
Jim Thompson <jimthompson5802@aol.com>
2019-07-19 19:32:22 -0400

    removed port specification

--- bin/run_python_container.sh
+++ bin/run_python_container.sh
@@ -3,7 +3,6 @@
 docker run -it --rm \
     -e MLFLOW_TRACKING_URI=${MLFLOW_TRACKING_URI} \
     -v ${KAGGLE_PROJECT}:/opt/project \
-    -p 5000:5000 \
     --name kag_python \
     kag_python \
     /bin/bash
