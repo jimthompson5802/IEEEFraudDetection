b01a3754c38835a597f74de938ac0ab30d0b0aaf
Jim Thompson <jimthompson5802@aol.com>
2019-07-21 19:43:59 -0400

    added definition for global parameters

--- utils/kaggle.py
+++ utils/kaggle.py
@@ -2,7 +2,27 @@
 #  Collection of Kaggle-related functions
 #
 
+import os
 from sklearn.metrics import roc_auc_score
+
+
+
+# global parameters
+def get_global_parameters():
+    inside_docker = os.getenv('INSIDE_DOCKER')
+
+    if inside_docker is not None:
+        global_parms = {
+            'PROJ_DIR': '/opt/project'
+        }
+    else:
+        global_parms = {
+            'PROJ_DIR': os.getcwd()
+        }
+
+    return global_parms
+
+
 
 # return metric for the contest
 def calc_contest_metric(y_true, y_score):
