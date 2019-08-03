0bea083d1473396da361bd432a7ea07a39c986c0
Jim Thompson <jimthompson5802@aol.com>
2019-08-03 11:24:55 -0400

    added port for dask ui

--- bin/run_jupyter_container.sh
+++ bin/run_jupyter_container.sh
@@ -5,6 +5,7 @@ docker run --detach --rm \
     -e INSIDE_DOCKER=true \
     -v ${KAGGLE_PROJECT}:/opt/project \
     -p 8888:8888 \
+    -p 8787:8787 \
     --name kag_jupyter \
     kag_python \
     jupyter notebook --no-browser --ip 0.0.0.0 \
