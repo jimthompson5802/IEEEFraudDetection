9395f70459c0e4f96fc9e6ccc00eb63dd3f49344
Jim Thompson <jimthompson5802@aol.com>
2019-08-18 08:57:39 -0400

    add other_run_parms to docker run command increated shm-size for mlbox to 1g

--- bin/run_image.sh
+++ bin/run_image.sh
@@ -9,11 +9,13 @@ case ${container_parm} in
     pybash) image=kag_python
             interactive='-it'
             ports=""
+            other_run_parms=''
             cmd=/bin/bash;;
 
     pyjpynb) image=kag_python
              interactive='--detach'
              ports='-p 8888:8888'
+             other_run_parms=''
              cmd="jupyter notebook --no-browser --ip 0.0.0.0 \
                 --allow-root --password='' --NotebookApp.token='' \
                  --notebook-dir=/opt/project";;
@@ -21,11 +23,13 @@ case ${container_parm} in
     mlboxbash) image=kag_mlbox
             interactive='-it'
             ports=""
+            other_run_parms=''
             cmd=/bin/bash;;
 
     mlboxjpynb) image=kag_mlbox
              interactive='--detach'
              ports='-p 8889:8889'
+             other_run_parms='--shm-size=1g'
              cmd="jupyter notebook --no-browser --ip 0.0.0.0  --port 8889\
                 --allow-root --password='' --NotebookApp.token='' \
                  --notebook-dir=/opt/project";;
@@ -35,11 +39,13 @@ case ${container_parm} in
     h2obash) image=kag_h2o
             interactive='-it'
             ports=""
+            other_run_parms=''
             cmd=/bin/bash;;
 
     h2ojpynb) image=kag_h2o
              interactive='--detach'
              ports='-p 8890:8890'
+             other_run_parms=''
              cmd="jupyter notebook --no-browser --ip 0.0.0.0 --port 8890\
                 --allow-root --password='' --NotebookApp.token='' \
                  --notebook-dir=/opt/project";;
@@ -47,11 +53,13 @@ case ${container_parm} in
     tpotbash) image=kag_tpot
             interactive='-it'
             ports=""
+            other_run_parms=''
             cmd=/bin/bash;;
 
     tpotjpynb) image=kag_tpot
              interactive='--detach'
              ports='-p 8891:8891'
+             other_run_parms=''
              cmd="jupyter notebook --no-browser --ip 0.0.0.0 --port 8891\
                 --allow-root --password='' --NotebookApp.token='' \
                  --notebook-dir=/opt/project";;
@@ -69,6 +77,7 @@ docker run ${interactive} --rm \
     -e INSIDE_DOCKER=true \
     -v ${KAGGLE_PROJECT}:/opt/project \
     ${ports} \
+    ${other_run_parms} \
     --name ${container_parm} \
     ${image} \
     ${cmd}
