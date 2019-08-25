468a42b0ba13feaf70f15653e9235e39f52114cc
Jim Thompson <jimthompson5802@aol.com>
2019-08-25 08:45:23 -0400

    initial check-in for bash scripts to push/pull dockerhub

---
+++ bin/push_images.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# push docker images to dockerhub
+# assumes already logged into dockerhub
+
+# images to push
+images=(
+    kag_python
+    kag_mlbox
+    kag_h2o
+    kag_tpot
+)
+
+# function to push an image to docker hub
+push_image () {
+
+    image_to_push=${1}
+    echo "pushing image: " ${image_to_push}
+
+    # create tag for docker hub
+    docker tag ${image_to_push} dsimages/${image_to_push}
+
+    # push to docker hub
+    docker push dsimages/${image_to_push}
+
+}
+
+
+#
+#  push images
+#
+for image in ${images[@]}
+do
+    push_image ${image}
+done
