e4bdce34b18715150b7cae04fd8e2c41ad8818e4
Jim Thompson <jimthompson5802@aol.com>
2019-08-31 00:07:00 -0400

    initial check-in

---
+++ utils/purge_deleted_mlflow_runs.py
@@ -0,0 +1,27 @@
+import yaml
+import shutil
+import os.path
+import glob
+import mlflow
+
+#%%
+# retrieve location for mlflow tracking
+TRACKING_DIR = mlflow.get_tracking_uri()
+
+#%%
+# retrieve experiment directories
+experiment_dirs = glob.glob(os.path.join(TRACKING_DIR,'[0-9]'))
+
+#%%
+# cycle through run directories looking for deleted runs
+for run_dir in [run_dir for d in experiment_dirs for run_dir in glob.glob(os.path.join(d, '[a-f0-9]*'))]:
+
+    # retrieve run meta data file
+    with open(os.path.join(run_dir,'meta.yaml'), 'r') as f:
+        run_meta_data = yaml.safe_load(f)
+
+    if run_meta_data['lifecycle_stage'] == 'deleted':
+        print('purging: ', run_dir)
+        shutil.rmtree(run_dir)
+
+
