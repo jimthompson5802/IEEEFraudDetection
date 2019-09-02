9ff1426c783b21f3c9a4b98720d4455e8f63ba75
Jim Thompson <jimthompson5802@aol.com>
2019-09-02 17:42:24 -0400

    added start/end messages

--- utils/purge_deleted_mlflow_runs.py
+++ utils/purge_deleted_mlflow_runs.py
@@ -14,6 +14,7 @@ experiment_dirs = glob.glob(os.path.join(TRACKING_DIR,'[0-9]'))
 
 #%%
 # cycle through run directories looking for deleted runs
+print('starting purge run')
 for run_dir in [run_dir for d in experiment_dirs for run_dir in glob.glob(os.path.join(d, '[a-f0-9]*'))]:
 
     # retrieve run meta data file
@@ -24,4 +25,5 @@ for run_dir in [run_dir for d in experiment_dirs for run_dir in glob.glob(os.pat
         print('purging: ', run_dir)
         shutil.rmtree(run_dir)
 
+print('completed purge run')
 
